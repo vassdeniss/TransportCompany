c6b1a90feeb27512daf88d655cfe1a818b598974
Denis Vasilev <vassdeniss@gmail.com>
2024-12-10 10:55:49 +0200

    bug: bug fix

--- src/main/java/org/f108349/denis/entity/validation/Phone.java
+++ src/main/java/org/f108349/denis/entity/validation/Phone.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = RegistrationNumberValidator.class)
+@Constraint(validatedBy = PhoneValidator.class)
 @Target({ ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Phone {
