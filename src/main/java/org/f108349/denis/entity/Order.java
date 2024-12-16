ec5450877dbe7533dd5fa99bb6979a04107beaed
Denis Vasilev <vassdeniss@gmail.com>
2024-12-16 23:17:35 +0200

    feat: order entity tests

--- src/main/java/org/f108349/denis/entity/Order.java
+++ src/main/java/org/f108349/denis/entity/Order.java
@@ -1,36 +1,62 @@
 package org.f108349.denis.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.f108349.denis.entity.enums.Status;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.sql.Date;
+import java.util.UUID;
 
 @Entity
 @Table(name = "`order`")
 public class Order {
+    public Order() {}
+
+    public Order(Date orderDate, Date shipmentDate, String destination, 
+                 BigDecimal totalCost, BigDecimal totalWeight) {
+        this.id = UUID.randomUUID().toString();
+        this.orderDate = orderDate;
+        this.shipmentDate = shipmentDate;
+        this.destination = destination;
+        this.totalCost = totalCost;
+        this.totalWeight = totalWeight;
+        this.status = Status.PENDING;
+    }
+
     @Id
     @Column(columnDefinition = "CHAR(36)")
     private String id;
     
+    @NotNull(message = "Order date cannot be null.")
     @Column(name = "order_date", nullable = false)
-    private LocalDateTime orderDate;
+    private Date orderDate;
 
     @Column(name = "shipment_date")
-    private LocalDateTime shipmentDate;
+    private Date shipmentDate;
 
+    @NotBlank(message = "Destination cannot be blank.")
+    @Size(min = 3, max = 255, message = "Destination must be between 3 and 255 characters.")
     @Column(nullable = false)
     private String destination;
     
+    @NotNull(message = "Total cost cannot be null.")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Total cost must be greater than zero.")
     @Column(name = "total_cost", nullable = false, precision = 10, scale = 2)
     private BigDecimal totalCost;
 
-    @Column(name = "total_weigth", nullable = false, precision = 10, scale = 2)
+    @NotNull(message = "Total weight cannot be null.")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Total weight must be greater than zero.")
+    @Column(name = "total_weight", nullable = false, precision = 10, scale = 2)
     private BigDecimal totalWeight;
 
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted;
     
+    @NotNull(message = "Status cannot be null.")
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Status status = Status.PENDING;
@@ -50,4 +76,100 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "vehicle_id", foreignKey = @ForeignKey(name = "FK_vehicle_order"))
     private Vehicle vehicle;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Date getShipmentDate() {
+        return shipmentDate;
+    }
+
+    public void setShipmentDate(Date shipmentDate) {
+        this.shipmentDate = shipmentDate;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public BigDecimal getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(BigDecimal totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public BigDecimal getTotalWeight() {
+        return totalWeight;
+    }
+
+    public void setTotalWeight(BigDecimal totalWeight) {
+        this.totalWeight = totalWeight;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
 }
---
+++ src/test/java/org/f108349/denis/entity/OrderEntityTests.java
@@ -0,0 +1,123 @@
+package org.f108349.denis.entity;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OrderEntityTests {
+    @Test
+    public void testOrderDate_whenNull_shouldReturnConstraintViolations() {
+        // Arrange
+        Order order = new Order(null, Date.valueOf("2024-12-16"), "Some destination", 
+                BigDecimal.valueOf(100), BigDecimal.valueOf(10));
+
+        // Act
+        List<String> messages = EntityHelper.validate(order);
+
+        // Assert
+        assertEquals(1, messages.size());
+        assertTrue(messages.contains("Order date cannot be null."));
+    }
+    
+    @Test
+    public void testDestination_whenReceedsMinLength_shouldReturnConstraintViolations() {
+        // Arrange
+        Order order = new Order(Date.valueOf("2024-12-16"), Date.valueOf("2024-12-18"), "aa", 
+                BigDecimal.valueOf(100), BigDecimal.valueOf(10));
+
+        // Act
+        List<String> messages = EntityHelper.validate(order);
+
+        // Assert
+        assertEquals(1, messages.size());
+        assertTrue(messages.contains("Destination must be between 3 and 255 characters."));
+    }
+    
+    @Test
+    public void testDestination_whenExceedsMaxLength_shouldReturnConstraintViolations() {
+        // Arrange
+        Order order = new Order(Date.valueOf("2024-12-16"), Date.valueOf("2024-12-18"), "a".repeat(256), 
+                BigDecimal.valueOf(100), BigDecimal.valueOf(10));
+
+        // Act
+        List<String> messages = EntityHelper.validate(order);
+
+        // Assert
+        assertEquals(1, messages.size());
+        assertTrue(messages.contains("Destination must be between 3 and 255 characters."));
+    }
+    
+    @Test
+    public void testTotalCost_whenNull_shouldReturnConstraintViolations() {
+        // Arrange
+        Order order = new Order(Date.valueOf("2024-12-16"), Date.valueOf("2024-12-18"), "Some destination", 
+                null, BigDecimal.valueOf(10));
+
+        // Act
+        List<String> messages = EntityHelper.validate(order);
+
+        // Assert
+        assertEquals(1, messages.size());
+        assertTrue(messages.contains("Total cost cannot be null."));
+    }
+    
+    @Test
+    public void testTotalCost_whenZero_shouldReturnConstraintViolations() {
+        // Arrange
+        Order order = new Order(Date.valueOf("2024-12-16"), Date.valueOf("2024-12-18"), "Some destination", 
+                BigDecimal.valueOf(0), BigDecimal.valueOf(10));
+
+        // Act
+        List<String> messages = EntityHelper.validate(order);
+
+        // Assert
+        assertEquals(1, messages.size());
+        assertTrue(messages.contains("Total cost must be greater than zero."));
+    }
+    
+    @Test
+    public void testTotalWeight_whenNull_shouldReturnConstraintViolations() {
+        // Arrange
+        Order order = new Order(Date.valueOf("2024-12-16"), Date.valueOf("2024-12-18"), "Some destination", 
+                BigDecimal.valueOf(15), null);
+
+        // Act
+        List<String> messages = EntityHelper.validate(order);
+
+        // Assert
+        assertEquals(1, messages.size());
+        assertTrue(messages.contains("Total weight cannot be null."));
+    }
+    
+    @Test
+    public void testTotalWeight_whenZero_shouldReturnConstraintViolations() {
+        // Arrange
+        Order order = new Order(Date.valueOf("2024-12-16"), Date.valueOf("2024-12-18"), "Some destination", 
+                BigDecimal.valueOf(10), BigDecimal.valueOf(0));
+
+        // Act
+        List<String> messages = EntityHelper.validate(order);
+
+        // Assert
+        assertEquals(1, messages.size());
+        assertTrue(messages.contains("Total weight must be greater than zero."));
+    }
+    
+    @Test
+    public void testOrder_whenValid_shouldNotReturnConstraintViolations() {
+        // Arrange
+        Order order = new Order(Date.valueOf("2024-12-16"), Date.valueOf("2024-12-18"), "Some destination", 
+                BigDecimal.valueOf(100), BigDecimal.valueOf(10));
+
+        // Act
+        List<String> messages = EntityHelper.validate(order);
+
+        // Assert
+        assertTrue(messages.isEmpty());
+    }
+}
